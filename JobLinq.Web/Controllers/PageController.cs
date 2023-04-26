02ddeb7f8f25aee0633630e0635368b21b9bc660
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-04-26 14:25:05 +0300

    Add project files.

---
+++ JobLinq.Web/Controllers/PageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobLinq.Web.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+    }
+}
